fd920447c3d7c2853f9b0c85b9dd7921ca01942f
bnbn131 <40466659+bnbn131@users.noreply.github.com>
2024-08-28 17:08:59 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,111 +1,78 @@
-import agents
-import jetbrains.buildServer.configs.kotlin.v10.toExtId
-import jetbrains.buildServer.configs.kotlin.v2019_2.AbsoluteId
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildStep
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.BuildFailureOnText
-import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.failOnText
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-import readScript
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCompose
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.projectFeatures.dockerRegistry
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
-class Docker (
-    private val jobName: String,
-    private val jobDesc: String,
-    private val vcsRepository: GitVcsRoot,
-    private val vcsBranchFilter: String,
-    private val parentFolderName: String,
-    private val depParJobId: String,
-    private val depArtifactJobId: String
-) : BuildType({
-    name = jobName
-    id("${jobName}_${parentFolderName}".toExtId())
-    description = jobDesc
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
 
-    vcs {
-        root(vcsRepository)
-        cleanCheckout = true
-        branchFilter = vcsBranchFilter
-    }
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
 
-    params {
-        param("app_type", "%dep.${depParJobId}.app_type%")
-        param("service_version", "%dep.${depParJobId}.service_version%")
-        param("build_artifacts", "docker")
-        param("DOCKER_BASE_IMAGE", "java/liberica-openjdk-alpine-musl:17.0.6")
-    }
-    steps {
-        script {
-            name = "Get cache"
-            scriptContent = readScript("files/scripts/build/backend/get_cache.sh")
-        }
-        script {
-            name = "Delete artifact from Nexus-CD"
-            scriptContent = readScript("files/scripts/build/delete_docker_artifact_from_nexus_cd.sh")
-        }
-        script {
-            name = "Docker login"
-            scriptContent = readScript("files/scripts/docker_login.sh")
-        }
-        script {
-            name = "Docker configs"
-            scriptContent = readScript("files/scripts/build/backend/docker_configs.sh")
-        }
-        script {
-            name = "Docker build"
-            scriptContent = readScript("files/scripts/build/backend/docker_build.sh")
-        }
-        script {
-            name = "Docker push"
-            scriptContent = readScript("files/scripts/build/docker_push.sh")
-        }
-        script {
-            name = "Associate tag with version with artifacts in Nexus-CD"
-            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
-            scriptContent = readScript("files/scripts/build/associate_tag_with_version_with_artifact_in_nexus_cd.sh")
+To debug settings scripts in command-line, run the
 
-            conditions {
-                equals("qg", "true")
-            }
-        }
-        script{
-            name = "Associate tag TO_DMZ with artifacts in Nexus-CD"
-            scriptContent = readScript("files/scripts/build/associate_tag_to_dmz_with_artifact_in_nexus_cd.sh")
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
 
-            conditions {
-                equals("deploy_to_dmz", "true")
-                equals("qg", "true")
-            }
-        }
-        script {
-            name = "Docker logout"
-            executionMode = BuildStep.ExecutionMode.ALWAYS
-            scriptContent = readScript("files/scripts/docker_logout.sh")
-        }
-    }
+command and attach your debugger to the port 8000.
 
-    dependencies {
-        artifacts(AbsoluteId(depArtifactJobId)) {
-            artifactRules = "**/*"
-        }
-    }
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2024.03"
+
+project {
+
+    vcsRoot(GitTest)
+
+    buildType(Build)
 
     features {
-        perfmon {
+        dockerRegistry {
+            id = "PROJECT_EXT_2"
+            name = "Docker Registry"
+            userName = "1"
+            password = "credentialsJSON:19b1d2c4-63a5-41f4-a648-675e9030edb0"
         }
     }
+}
 
-    failureConditions {
-        executionTimeoutMin = 15
-        failOnText {
-            conditionType = BuildFailureOnText.ConditionType.CONTAINS
-            pattern = "Out of system resources"
-            failureMessage = "Out of system resources"
-            reverse = false
-            stopBuildOnFailure = true
-        }
+object Build : BuildType({
+    name = "build"
+
+    params {
+        param("branch", "main")
+        param("git", "https://github.com/bnbn131/teamcity2.git")
     }
 
-    requirements { agents("build") }
+    vcs {
+        root(GitTest)
+    }
+
+    steps {
+        dockerCompose {
+            name = "docker run"
+            id = "docker_run"
+            executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+            file = """src\docker-compose.yml"""
+        }
+        gradle {
+            name = "cucumber run"
+            id = "cucumber_run"
+            tasks = "cucumber"
+        }
+    }
+})
+
+object GitTest : GitVcsRoot({
+    name = "git_test"
+    url = "%git%"
+    branch = "%branch%"
 })
